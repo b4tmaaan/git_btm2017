fc84d3fdae7b6c7caf2a8b66c4b2b61d61415301
b4tmaaan <CosimoJann@project-rav.com>
2017-11-22 16:10:54 +0000

    changed 3 files

--- file_manager.py
+++ file_manager.py
@@ -5,12 +5,15 @@ transaction_filename = "transactions.txt"
 
 def add_client(client_name):
     pass
-      
+
 def add_transaction(debtor, creditor, amount):
     pass
-        
+
 def get_clients():
     pass
 
 def get_transactions():
     pass
+
+def blabla():
+    print("hello World!")
--- functionalities.py
+++ functionalities.py
@@ -1,12 +1,13 @@
 import file_manager as fM
 
 def say_hi():
+    print("lalALALALALALA HAAAAlllo")
 
 def new_client():
     pass
-    
+
 def new_transaction():
     pass
-    
+
 def look_credit():
     pass
